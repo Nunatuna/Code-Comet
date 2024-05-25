9963bb93a8a8e5a62aee62f2b57909c9eb3f6040
Aiden <aiden.0407@hotmail.com>
2024-05-25 18:52:44 +0200

    Land ahoy! The end is in sight - Here's some final details
    +Glossary lists has been updated with new design
    +Fixed icon not showing up on pages
    +Download button added to glossary pages to be able to download entire glossary
    +Sign in/Login button group fixed so account logo will appeare instead when screen gets too small
    
    -Memes has been moved to Instagram

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.display = "none";
 }
+
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("accordion-active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
